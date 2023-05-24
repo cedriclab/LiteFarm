d15d3a1a1b10bbdca2f30106e1f7fc095903520f
cedriclab <cedric.levlab@gmail.com>
2023-05-24 09:20:08 -0400

    Added functions to create and store temporary tokens including JWT refresh tokens

--- packages/api/src/util/jwt.js
+++ packages/api/src/util/jwt.js
@@ -1,8 +1,14 @@
 import jwt from 'jsonwebtoken';
 
+import { client } from './redis';
+
+import { randomBytes } from 'crypto';
+
 const ACCESS_TOKEN_EXPIRES_IN = '7d';
 const RESET_PASSWORD_TOKEN_EXPIRES_IN = '1d';
 const SCHEDULER_TOKEN_EXPIRES_IN = '1d';
+
+const DEFAULT_RANDOM_TOKEN_LENGTH = 32;
 
 const tokenType = {
   access: process.env.JWT_SECRET,
@@ -19,6 +25,16 @@ const expireTime = {
   scheduler: SCHEDULER_TOKEN_EXPIRES_IN,
 };
 
+const SINGLE_USE_TOKEN_LENGTH = 256;
+const REFRESH_TOKEN_LENGTH = 128;
+
+const REFRESH_TOKEN_EXPIRATION = process.env.REFRESH_TOKEN_EXPIRATION || 7 * 24 * 60 * 60; // Defaults to 7 days - time is in SECONDS, not MS
+
+const SingleUseTokenType = {
+  REFRESH: 'REFRESH',
+  EMAIL: 'EMAIL',
+};
+
 function createToken(type, payload) {
   return jwt.sign(payload, tokenType[type], {
     expiresIn: expireTime[type],
@@ -26,4 +42,64 @@ function createToken(type, payload) {
   });
 }
 
-export { createToken, expireTime, tokenType };
+/**
+ * @function createRandomToken
+ * @description Creates a cryptographically safe random string of a given length
+ * @async
+ * @param {Number} length - the length of the token
+ * @returns {String}
+ */
+const createRandomToken = async (length = DEFAULT_RANDOM_TOKEN_LENGTH) => {
+  return new Promise((resolve, reject) => {
+    randomBytes(length, (err, buffer) => {
+      if (err) {
+        return reject(err);
+      }
+
+      const token = buffer.toString('hex');
+
+      return resolve(token);
+    });
+  });
+};
+
+/**
+ * @function emitSingleUseToken
+ * @description Emits a single-use token and stores it in Redis
+ * @param {SingleUseTokenType} type
+ * @param {Object} info - the payload to go along with the key
+ * @param {Object} options - options; generally the length and the expiration
+ */
+const emitSingleUseToken = async (type, info, options) => {
+  const tokenLength = (options && options.length) || SINGLE_USE_TOKEN_LENGTH;
+
+  // create the random token
+  const token = await createRandomToken(tokenLength);
+
+  // Use the token and the type to create a redis key
+  const key = `token.${type.toLowerCase()}.${token}`;
+
+  // Here, we set the expiration for the token
+  const tokenExpiration = (options && options.expiration) || REFRESH_TOKEN_EXPIRATION;
+
+  // Store the token in redis, with expiration
+  await client.set(key, JSON.stringify(info), 'EX', tokenExpiration);
+
+  return token;
+};
+
+// Just a shorthand for emitSingleUseToken with REFRESH set as type and REFRESH_TOKEN_LENGTH as the length
+const emitRefreshToken = async (info) => {
+  return emitRefreshToken(SingleUseTokenType.REFRESH, info, { length: REFRESH_TOKEN_LENGTH });
+};
+
+export {
+  createToken,
+  expireTime,
+  tokenType,
+  SINGLE_USE_TOKEN_LENGTH,
+  REFRESH_TOKEN_LENGTH,
+  SingleUseTokenType,
+  emitRefreshToken,
+  emitSingleUseToken,
+};
