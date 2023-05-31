78d057e19cab116fa081f059a644c1309a6360a0
cedriclab <cedric.levlab@gmail.com>
2023-05-31 09:00:40 -0400

    Added token signature to prevent brute-force attacks on single-use tokens

--- packages/api/src/util/jwt.js
+++ packages/api/src/util/jwt.js
@@ -25,10 +25,12 @@ const expireTime = {
   scheduler: SCHEDULER_TOKEN_EXPIRES_IN,
 };
 
-const SINGLE_USE_TOKEN_LENGTH = 256;
+const SINGLE_USE_TOKEN_LENGTH = 128;
 const REFRESH_TOKEN_LENGTH = 128;
 
 const REFRESH_TOKEN_EXPIRATION = process.env.REFRESH_TOKEN_EXPIRATION || 7 * 24 * 60 * 60; // Defaults to 7 days - time is in SECONDS, not MS
+
+const SINGLE_USE_TOKEN_SECRET = process.env.SINGLE_USE_TOKEN_SECRET;
 
 const SingleUseTokenType = {
   REFRESH: 'REFRESH',
@@ -76,10 +78,13 @@ const createRandomToken = async (length = DEFAULT_RANDOM_TOKEN_LENGTH) => {
 
 /**
  * @function emitSingleUseToken
- * @description Emits a single-use token and stores it in Redis
+ * @description Emits a single-use token, stores it in Redis, and returns a SIGNED version
+ * @note We use a 2-step approach here: first, we create a random token, which we will store in Redis along with a payload in JSON format.  This Key/Value pair is set to expire after the provided validity period.  Then, we sign this token in the format of a JWT with the same expiration time, and we send that signed token out.
+ * @note Why not just use the random token OR the signed token?  The random token alone can be the target of a brute-force attack.  The signed token alone is harder to invalidate after use (because the tokens are single-use).  With the 2 steps, we have both security against brute-force attacks AND easy invalidation after use.
  * @param {SingleUseTokenType} type
  * @param {Object} info - the payload to go along with the key
  * @param {Object} options - options; generally the length and the expiration
+ * @returns {String} - a signed token, in the format of a JWT, which's payload contains { token, userId } which can then be used to retrieve a whole payload from redis
  */
 const emitSingleUseToken = async (type, info, options) => {
   const tokenLength = (options && options.length) || SINGLE_USE_TOKEN_LENGTH;
@@ -87,24 +92,54 @@ const emitSingleUseToken = async (type, info, options) => {
   // create the random token
   const token = await createRandomToken(tokenLength);
 
-  // Use the token and the type to create a redis key
-  const key = `token.${type.toLowerCase()}.${token}`;
+  const tokenPayload = { token, userId: info.user_id };
 
   // Here, we set the expiration for the token
   const tokenExpiration = (options && options.expiration) || REFRESH_TOKEN_EXPIRATION;
 
+  const signedToken = await jwt.sign(tokenPayload, SINGLE_USE_TOKEN_SECRET, {
+    expiresIn: `${tokenExpiration}s`,
+    algorithm: 'HS256',
+  });
+
+  // Use the token and the type to create a redis key
+  const key = `token.${type.toLowerCase()}.${token}`;
+
   // Store the token in redis, with expiration
   await client.set(key, JSON.stringify(info), 'EX', tokenExpiration);
 
-  return token;
+  return signedToken;
 };
 
 // Just a shorthand for emitSingleUseToken with REFRESH set as type and REFRESH_TOKEN_LENGTH as the length
 const emitRefreshToken = async (info) => {
-  return emitRefreshToken(SingleUseTokenType.REFRESH, info, { length: REFRESH_TOKEN_LENGTH });
+  return emitSingleUseToken(SingleUseTokenType.REFRESH, info, { length: REFRESH_TOKEN_LENGTH });
 };
 
-const validateSingleUseToken = async (type, token, invalidateToken = true) => {
+/**
+ * @function validateSingleUseToken
+ * @description This function is the inverse of emitSingleUseToken, except that, if provided with the optional argument invalidateToken = true, it will have the side-effect of invalidating the provided token
+ * @note IN NORMAL CASES, invalidateToken *should* be true because it is the mechanism whereby we can invalidate SINGLE-USE tokens after they have been used.
+ * @param {String} type
+ * @param {String} signedToken
+ * @param {Boolean} invalidateToken
+ * @returns {Object|Null}
+ */
+const validateSingleUseToken = async (type, signedToken, invalidateToken = true) => {
+  const parsedPayload = await jwt.verify(signedToken, SINGLE_USE_TOKEN_SECRET).catch((error) => {
+    console.error(
+      `Could not decrypt signed single-use token because [likely it has expired]->`,
+      error,
+    );
+    return null;
+  });
+
+  if (!parsedPayload) {
+    return null;
+  }
+
+  const { token } = parsedPayload;
+
   // Use the token and the type to create a redis key
   const key = `token.${type.toLowerCase()}.${token}`;
 
@@ -114,6 +149,11 @@ const validateSingleUseToken = async (type, token, invalidateToken = true) => {
     // By default, we invalidate the token once it has been validated because they are single-use
     if (invalidateToken) {
       await client.del(key);
+    } else {
+      // Just a pesky warning
+      console.warn(
+        `The "validateSingleUseToken" with the invalidateToken argument set to false.  Be careful!  This is how we make sure single-use tokens are, well, single-use.`,
+      );
     }
 
     try {
